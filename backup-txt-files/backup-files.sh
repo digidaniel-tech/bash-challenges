be01979416b3cbbb63df347aa006a03e705b5df1
Daniel Wollbro <wollbro90@gmail.com>
2024-08-24 22:16:12 +0200

    Using {} for variables is good-practices and using local on variables in functions isolates variables

--- backup-txt-files/backup-files.sh
+++ backup-txt-files/backup-files.sh
@@ -5,31 +5,31 @@
 
   # Function used to write to log
   write_log () {
-    DATETIME_NOW=$(date "+%Y-%m-%d %H:%M:%S")
-    echo "[$DATETIME_NOW] $1" >> $LOG_FILE
+    local DATETIME_NOW=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "[${DATETIME_NOW}] ${1}" >> ${LOG_FILE}
   }
 
   # Function used to print output to console
   print_console () {
     for arg in "$@"; do
-      echo ":: $arg"
+      echo ":: ${arg}"
     done
   }
 
   # Function to create backup folder if not already exists
   generate_backup_folder () {
-    DATE_NOW=$(date "+%Y%m%d")
-    BACKUP_FOLDER="backup_$DATE_NOW"
+    local DATE_NOW=$(date "+%Y%m%d")
+    local BACKUP_FOLDER="backup_${DATE_NOW}"
 
-    if [[ ! -d $BACKUP_FOLDER ]]; then
-      mkdir $BACKUP_FOLDER
+    if [[ ! -d ${BACKUP_FOLDER} ]]; then
+      mkdir ${BACKUP_FOLDER}
     fi
 
-    echo $BACKUP_FOLDER
+    echo ${BACKUP_FOLDER}
   }
 
   # Check so folder path is entered as argument
-  if [[ -z $FOLDER_PATH ]]; then
+  if [[ -z ${FOLDER_PATH} ]]; then
     print_console "[ERROR] No folder is specified as argument, \
   please enter folder to backup"
 
@@ -40,31 +40,31 @@
   BACKUP_FOLDER=$(generate_backup_folder)
 
   # Check if folder is empty then do not try to backup anything
-  if [ -z "$(ls -A "$FOLDER_PATH")" ]; then
-    write_log "No files found in $FOLDER_PATH to backup."
+  if [ -z "$(ls -A "${FOLDER_PATH}")" ]; then
+    write_log "No files found in ${FOLDER_PATH} to backup."
     exit 0
   fi
 
   # Trim end slashes 
-  FOLDER_PATH="$(echo "$FOLDER_PATH" | sed 's:/*$::')"
+  FOLDER_PATH="$(echo "${FOLDER_PATH}" | sed 's:/*$::')"
 
   # Loop trough files and back them up
-  for FILE in "$FOLDER_PATH"/*; do
+  for FILE in "${FOLDER_PATH}"/*; do
     # Make sure it is a file and not a directory
-    if [[ ! -f "$FILE" ]]; then
+    if [[ ! -f "${FILE}" ]]; then
       continue
     fi
 
     # Normalize filepath to stripout trailing slashes (ex. example-files/)
-    FILE_NAME=$(basename "$FILE")
+    FILE_NAME=$(basename "${FILE}")
 
     # Copy file to backup folder
-    if cp "$FILE" "$BACKUP_FOLDER/$FILE_NAME"; then
+    if cp "${FILE}" "${BACKUP_FOLDER}/${FILE_NAME}"; then
       # Write to log that the file has been backed up
-      write_log "Backup of $FILE done."
+      write_log "Backup of ${FILE} done."
     else
       # Write to log that the file has failed to be backed up
-      write_log "[ERROR] Failed to backup $FILE."
+      write_log "[ERROR] Failed to backup ${FILE}."
     fi
   done
 
